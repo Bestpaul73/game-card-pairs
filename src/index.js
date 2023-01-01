93e5bc20c5bd78d418d1f4edb668fe0f9f6f5c9c
Bestpaul <bestpaul@yandex.ru>
2023-01-01 15:11:35 +0300

    01 jan

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 /* eslint-disable no-unused-vars */
+import './template-engine';
+import './level';
+import './game';
+import './style.css';
+
+
 let LEVEL = '';
 const APP = document.querySelector('.app');
 
